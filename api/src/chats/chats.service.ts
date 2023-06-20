135306734c78e306bf7db2acd80be418ba583d41
zhaozhiming <zhaozhiming@users.noreply.github.com>
2023-06-20 18:32:39 +0800

    feat: add prompt check

--- api/src/chats/chats.service.ts
+++ api/src/chats/chats.service.ts
@@ -1,33 +1,55 @@
-import { Injectable, Logger, MessageEvent } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { HNSWLib } from 'langchain/vectorstores/hnswlib';
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { OpenAI } from 'langchain/llms/openai';
 import { ChatOpenAI } from 'langchain/chat_models/openai';
 import {
   SystemMessagePromptTemplate,
   HumanMessagePromptTemplate,
   ChatPromptTemplate,
+  PromptTemplate,
 } from 'langchain/prompts';
+
 import { LLMChain } from 'langchain/chains';
+import { VECTOR_STORE_DIRECTORY } from '../common/constants';
 
 export interface QaResult {
   answer: string;
-  links: string[];
+  links?: string[];
 }
 
 @Injectable()
 export class ChatsService {
   private readonly logger = new Logger(ChatsService.name);
-  private chain = null;
+  private checkChain = null;
+  private chatChain = null;
 
   constructor(private configService: ConfigService) {
-    const model = new ChatOpenAI({
+    const checkModel = new OpenAI({
+      openAIApiKey: process.env.CHATGPT_APIKEY,
+      temperature: 0,
+      maxTokens: 5,
+    });
+    const template = `
+    Your are a helpful assistant for the expert in Chinese law. 
+    Please help me to judge whether the user question is related to the law. 
+    If yes, please answer 'Y', if not, please answer 'N'. 
+    Just reply one character, do not output additional information.
+    user question: {question}`;
+    const checkPrompt = new PromptTemplate({
+      template: template,
+      inputVariables: ['question'],
+    });
+    this.checkChain = new LLMChain({ llm: checkModel, prompt: checkPrompt });
+
+    const chatModel = new ChatOpenAI({
       openAIApiKey: process.env.CHATGPT_APIKEY,
       temperature: 0.9,
       maxTokens: 500,
     });
 
-    const prompt = ChatPromptTemplate.fromPromptMessages([
+    const chatPrompt = ChatPromptTemplate.fromPromptMessages([
       SystemMessagePromptTemplate.fromTemplate(
         `你是一位法律专家，对中国的国家法律特别熟悉，请基于以下资料回答用户的问题。
         如果用户的问题跟法律没有关系，请礼貌地拒绝回答。
@@ -36,17 +58,27 @@ export class ChatsService {
       ),
       HumanMessagePromptTemplate.fromTemplate('{question}'),
     ]);
-    this.chain = new LLMChain({ llm: model, prompt: prompt });
+    this.chatChain = new LLMChain({ llm: chatModel, prompt: chatPrompt });
+  }
+
+  async checkPrompt(prompt: string): Promise<boolean> {
+    const res = await this.checkChain.call({ question: prompt });
+    return res.text.includes('Y');
   }
 
   async chat(prompt: string): Promise<QaResult> {
-    const directory = 'vector-store';
+    const isLawQuestion = await this.checkPrompt(prompt);
+    if (!isLawQuestion) {
+      return {
+        answer: '很抱歉，作为法律专家，我无法回答跟法律无关的问题。',
+      };
+    }
     const loadedVectorStore = await HNSWLib.load(
-      directory,
+      VECTOR_STORE_DIRECTORY,
       new OpenAIEmbeddings()
     );
     const links = await loadedVectorStore.similaritySearch(prompt, 2);
-    const res = await this.chain.call({
+    const res = await this.chatChain.call({
       question: prompt,
       context: links.map(link => link.pageContent).join('\n'),
     });
--- api/src/scripts/vector-save.ts
+++ api/src/scripts/vector-save.ts
@@ -2,26 +2,29 @@ import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 import { CharacterTextSplitter } from 'langchain/text_splitter';
 import { HNSWLib } from 'langchain/vectorstores/hnswlib';
 import { TextLoader } from 'langchain/document_loaders/fs/text';
+import { VECTOR_STORE_DIRECTORY } from '../common/constants';
 
-const embeddings = new OpenAIEmbeddings();
 const splitter = new CharacterTextSplitter({
   chunkSize: 100,
   chunkOverlap: 5,
 });
 
 const main = async () => {
+  console.log('加载文档...');
   const loader = new TextLoader('source-docs/labour-law.txt');
   const file = await loader.load();
   const docs = await splitter.splitDocuments(file);
-  console.log({ length: docs.length });
+  console.log(`分割文档个数：${docs.length}`);
 
-  const vectorStore = await HNSWLib.fromDocuments(docs, embeddings);
-  const directory = 'vector-store';
+  const vectorStore = await HNSWLib.fromDocuments(
+    docs,
+    new OpenAIEmbeddings({
+      openAIApiKey: process.env.OPENAI_API_KEY,
+    })
+  );
+  const directory = VECTOR_STORE_DIRECTORY;
   await vectorStore.save(directory);
-
-  const loadedVectorStore = await HNSWLib.load(directory, embeddings);
-  const result = await loadedVectorStore.similaritySearch('加班超过8小时', 1);
-  console.log('result:', result);
+  console.log('加载完成');
 };
 
 main();
