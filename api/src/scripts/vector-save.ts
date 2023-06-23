22948a72a20aeba2d820c97d89306b1d19836c7f
zhaozhiming <zhaozhiming@users.noreply.github.com>
2023-06-23 10:33:51 +0800

    fix: add new docs files and embedding them

--- api/src/scripts/vector-save.ts
+++ api/src/scripts/vector-save.ts
@@ -2,8 +2,10 @@ import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 import { CharacterTextSplitter } from 'langchain/text_splitter';
 import { HNSWLib } from 'langchain/vectorstores/hnswlib';
 import { TextLoader } from 'langchain/document_loaders/fs/text';
+import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
 import { VECTOR_STORE_DIRECTORY } from '../common/constants';
 
+const embeddings = new OpenAIEmbeddings();
 const splitter = new CharacterTextSplitter({
   chunkSize: 100,
   chunkOverlap: 5,
@@ -11,17 +13,16 @@ const splitter = new CharacterTextSplitter({
 
 const main = async () => {
   console.log('加载文档...');
-  const loader = new TextLoader('source-docs/labour-law.txt');
-  const file = await loader.load();
-  const docs = await splitter.splitDocuments(file);
+  const loader = new DirectoryLoader('source-docs', {
+    '.txt': filePath => new TextLoader(filePath),
+  });
+  const sourceDocs = await loader.load();
+  console.log(`原始文档个数：${sourceDocs.length}`);
+
+  const docs = await splitter.splitDocuments(sourceDocs);
   console.log(`分割文档个数：${docs.length}`);
 
-  const vectorStore = await HNSWLib.fromDocuments(
-    docs,
-    new OpenAIEmbeddings({
-      openAIApiKey: process.env.OPENAI_API_KEY,
-    })
-  );
+  const vectorStore = await HNSWLib.fromDocuments(docs, embeddings);
   const directory = VECTOR_STORE_DIRECTORY;
   await vectorStore.save(directory);
   console.log('加载完成');
